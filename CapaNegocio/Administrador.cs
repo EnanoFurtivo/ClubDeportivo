f4a75d4a82fce4a6b24897de116861e0f13a3525
Bruno <Bruno@DESKTOP-S9HAOKP>
2022-03-30 20:51:52 -0300

    Agregue clase CuentaCorriente y Administrador con atributos y metodos

---
+++ CapaNegocio/Administrador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public class Administrador
+    {
+        
+        public Administrador()
+        {
+        }
+
+        public void AbmSocios(Socio socio)
+        {
+            
+        }
+
+        public void AbmProfesores(Profesor profesor)
+        {
+
+        }
+
+        public void AbmActividades(Actividad actividad)
+        {
+
+        }
+
+        public void RegistrarPago(double pago)
+        {
+
+        }
+
+        public void AsignarActividad(Profesor profesor, Socio socio, Actividad actividad)
+        {
+
+        }
+
+        public void DesvincularActividad(Profesor profesor, Socio socio, Actividad actividad)
+        {
+
+        }
+    }
+}
