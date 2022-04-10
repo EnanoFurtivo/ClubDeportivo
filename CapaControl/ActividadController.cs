9ac63ec4b54a20de4f262e575422dfb7e2c9a62a
Bruno <Bruno@DESKTOP-S9HAOKP>
2022-04-10 17:36:32 -0300

    Agregue ActividadesController

---
+++ CapaControl/ActividadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaNegocio;
+
+namespace CapaControl
+{
+    class ActividadesController
+    {
+        List<Actividad> ListActividad = new List<Actividad>();
+
+        public ActividadesController()
+        {
+
+        }
+
+        public bool AgregarActividad(Actividad actividad)
+        {
+            this.ListActividad.Add(actividad);
+        }
+
+        public bool EliminarActividad(Actividad actividad)
+        {
+            this.ListActividad.Remove(actividad);
+        }
+
+        public void ModificarActividad(Actividad actividad)
+        {
+            
+        }
+    }
+}
