e4bee069abfac056475f1924728dc4d2497cf511
Luciano Godoy <lucianogodoy@protonmail.com>
2022-04-10 19:12:46 -0300

    Add UsuarioController, Update ususuario socio etc...

---
+++ CapaControl/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using CapaNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaControl
+{
+    public abstract class UsuarioController
+    {
+        private List<Usuario> ListaUsuario = new List<Usuario>();
+
+        public abstract void AgregarUsuario();
+        
+        public bool ValidarCredenciales(int dni, string clave)
+        {
+            bool result = false;
+            Usuario u = null;
+
+            if ((u = this.GetUsuario(dni)) == null)
+                return false;
+
+            if (!u.ValidarClave(clave))
+                return false;
+
+            return result;
+        }
+
+        public Usuario GetUsuario(int dni)
+        {
+            return this.ListaUsuario.Find(u => u.Dni == dni);
+        }
+    }
+}
